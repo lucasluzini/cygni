9d20e5ef3626a018b6439a4e3a0d8d18264a07da
lzni <lucasluzini@hotmail.com>
2016-04-29 21:26:34 -0300

    coment2

---
+++ index.php
@@ -0,0 +1,61 @@
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <title>Cygni - Controle de Patrimônio</title>
+
+      <link href="arquivos/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="arquivos/css/estilo.css" rel="stylesheet">
+      <link href="arquivos/css/3.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+    <?php include_once("paginas/menu.html"); ?>
+
+
+            
+        <!-- Page content -->
+        <div id="page-content-wrapper">
+          <!-- Keep all page content within the page-content inset div! -->
+          <div class="page-content inset">
+              <div class="row">
+                <div class="col-md-12">
+                <p class="well lead">Cygni - Controle de Patrimônio</p>
+
+               <div class="container">
+                  <div class="row"> <!-- div da esquerda -->
+                      <!-- Text input CNPJ e Razao Social-->
+                      <div class="col-sm-8 contact-form"> <!-- div da direita -->
+                          <form id="contact" method="post" class="form" role="form">
+                              
+                          </form>
+                      </div> <!-- fim div da direita -->
+                  </div> <!-- fim div da esquerda -->
+              </div>
+                <p class="well lead">Progração para Internet - Si5N - Senac</p> 
+              </div>
+            </div>
+          </div>
+      </div>
+        
+  </div>
+
+
+
+
+
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="arquivos/bootstrap/js/jquery-2.2.3.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="arquivos/bootstrap/js/bootstrap.min.js"></script>
+      <script src="arquivos/js/menu.js"></script>
+      <script src="arquivos/js/showride.js"></script>
+
+
+    </body>
+  </html>
