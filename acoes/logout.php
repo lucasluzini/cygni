3562d418e07cbe2af043b8e7c4c49bc6365a6fad
lzni <lucasluzini@hotmail.com>
2016-05-22 17:30:15 -0300

    rm2

--- acoes/logout.php
+++
@@ -1,9 +0,0 @@
-<?php require "../acoes/verifica.php"; ?>
-<?php 
-// Inicia sessões, para assim poder destruí-las 
-session_start(); 
-session_destroy(); 
-
-header("Location: ../login.php"); 
-?>
-
