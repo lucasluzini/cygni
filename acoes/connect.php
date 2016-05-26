7e42aae960c5b03194bd4d9df4d9abfcd154f4fe
lzni <lucasluzini@hotmail.com>
2016-05-26 11:05:18 -0300

    rm

--- acoes/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servidor = "host=localhost port=5432 dbname=cygni user=postgres password=root";
-$usuario = "postgres";
-$senha = "root";
-
-$conexao = pg_connect($servidor) or die ("Nao foi possivel conectar ao servidor PostGreSQL");
-?>
